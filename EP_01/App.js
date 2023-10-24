37337001b91b779712dc400d3f702737ed48328b
akash03 <akash03@nagarro.com>
2023-10-24 15:07:25 +0530

    ep-1 part 4

--- EP_01/App.js
+++ EP_01/App.js
@@ -1,8 +1,21 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" }, // attribute
-  "HI EVERYONE !!"
+//nested elements
+/**
+ * <div id="parent">
+ *  <div id="child">
+ *      <h1>I'm h1 tag</h1>
+ *  </div>
+ * </div>
+ *
+ * ReactElement(Object) => HTML (browser understands)
+ */
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm H1 tag"),
+    React.createElement("h3", {}, "I'm H3 tag"),
+  ])
 );
-console.log(heading); // object
+console.log(parent); // object
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
